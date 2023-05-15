3bf614166b900fd8ede6ab14898df959ec4fb394
Edmir Galvan <edmirgalvaz@gmail.com>
2023-05-15 11:32:57 -0600

    set up react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { HomePage } from "./pages/HomePage";
+import { RegisterOutputPage } from "./pages/RegisterOutputPage";
+import { RegisterSpeakerPage } from "./pages/RegisterSpeakerPage";
+import { RegisterSpeechPage } from "./pages/RegisterSpeechPage";
+import { RegisterVisitPage } from "./pages/RegisterVisitPage";
+import { SpeakerPage } from "./pages/SpeakerPage";
+import { SpeakersPage } from "./pages/SpeakersPage";
+import { VisitsPage } from "./pages/VisitsPage";
+
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/register-output",
+    element: <RegisterOutputPage />,
+  },
+  {
+    path: "/register-speaker",
+    element: <RegisterSpeakerPage />,
+  },
+  {
+    path: "/register-speech",
+    element: <RegisterSpeechPage />,
+  },
+  {
+    path: "/register-visit",
+    element: <RegisterVisitPage />,
+  },
+  {
+    path: "/speaker/:id",
+    element: <SpeakerPage />,
+  },
+  {
+    path: "/speakers",
+    element: <SpeakersPage />,
+  },
+  {
+    path: "/visits",
+    element: <VisitsPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
