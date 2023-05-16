ded1535691a9d9a0d0965841156074f3b13e0679
Edmir Galvan <edmirgalvaz@gmail.com>
2023-05-15 18:40:19 -0600

    Redirect to register visit page

--- src/pages/VisitsPage/VisitsPage.tsx
+++ src/pages/VisitsPage/VisitsPage.tsx
@@ -1,15 +1,21 @@
 import { PrimaryButton } from "../../components/Buttons/Buttons";
 import { BackButton } from "../../components/BackButton/BackButton";
-
-import "./VisitsPage.css";
 import { VisitCard } from "../../components/VisitCard/VisitCard";
 
+import { useNavigate } from "react-router-dom";
+
+import "./VisitsPage.css";
+
 export const VisitsPage = () => {
+  const navigate = useNavigate();
+
   return (
     <section className="container-page visitsPage">
       <BackButton />
       <h1 className="title">Visitas</h1>
-      <PrimaryButton>Nueva visita</PrimaryButton>
+      <PrimaryButton onClick={() => navigate("/register-visit")}>
+        Nueva visita
+      </PrimaryButton>
       <section className="table">
         <VisitCard
           speech="¿Cómo ser amigos de Jehová?"
