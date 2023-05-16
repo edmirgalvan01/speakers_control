df1affdc0143059c6ec00a82f57e7d040cf9af26
Edmir Galvan <edmirgalvaz@gmail.com>
2023-05-15 18:36:05 -0600

    VisitCard created

---
+++ src/components/VisitCard/VisitCard.tsx
@@ -0,0 +1,51 @@
+import {
+  BiMicrophone,
+  BiUser,
+  BiCalendar,
+  BiHome,
+  BiMusic,
+} from "react-icons/bi";
+
+import "./VisitCard.css";
+
+interface Props {
+  speech: string;
+  speaker: string;
+  date: string;
+  congregation: string;
+  song: string;
+}
+
+export const VisitCard = ({
+  speech,
+  speaker,
+  date,
+  congregation,
+  song,
+}: Props) => {
+  return (
+    <div className="visitCard">
+      <div className="visitCard--item">
+        <BiMicrophone color="345996" size="20" />
+        <p>{speech}</p>
+      </div>
+      <div className="visitCard--item">
+        <BiUser color="345996" size="20" />
+        <p>{speaker}</p>
+      </div>
+      <div className="visitCard--item">
+        <BiCalendar color="345996" size="20" />
+        <p>{date}</p>
+      </div>
+      <div className="visitCard--item">
+        <BiHome color="345996" size="20" />
+        <p>{congregation}</p>
+      </div>
+      <div className="visitCard--item">
+        <BiMusic color="345996" size="20" />
+        <p>{song}</p>
+      </div>
+      <div className="divider"></div>
+    </div>
+  );
+};
--- src/pages/VisitsPage/VisitsPage.tsx
+++ src/pages/VisitsPage/VisitsPage.tsx
@@ -1,14 +1,8 @@
-import {
-  BiMicrophone,
-  BiUser,
-  BiCalendar,
-  BiHome,
-  BiMusic,
-} from "react-icons/bi";
 import { PrimaryButton } from "../../components/Buttons/Buttons";
+import { BackButton } from "../../components/BackButton/BackButton";
 
 import "./VisitsPage.css";
-import { BackButton } from "../../components/BackButton/BackButton";
+import { VisitCard } from "../../components/VisitCard/VisitCard";
 
 export const VisitsPage = () => {
   return (
@@ -17,75 +11,27 @@ export const VisitsPage = () => {
       <h1 className="title">Visitas</h1>
       <PrimaryButton>Nueva visita</PrimaryButton>
       <section className="table">
-        <div className="visitCard">
-          <div className="visitCard--item">
-            <BiMicrophone color="345996" size="20" />
-            <p>The Walt Disney Company</p>
-          </div>
-          <div className="visitCard--item">
-            <BiUser color="345996" size="20" />
-            <p>Ronald Richards</p>
-          </div>
-          <div className="visitCard--item">
-            <BiCalendar color="345996" size="20" />
-            <p>10/28/12</p>
-          </div>
-          <div className="visitCard--item">
-            <BiHome color="345996" size="20" />
-            <p>MasterCard</p>
-          </div>
-          <div className="visitCard--item">
-            <BiMusic color="345996" size="20" />
-            <p>Kiribati</p>
-          </div>
-          <div className="divider"></div>
-        </div>
-        <div className="visitCard">
-          <div className="visitCard--item">
-            <BiMicrophone color="345996" size="20" />
-            <p>The Walt Disney Company</p>
-          </div>
-          <div className="visitCard--item">
-            <BiUser color="345996" size="20" />
-            <p>Ronald Richards</p>
-          </div>
-          <div className="visitCard--item">
-            <BiCalendar color="345996" size="20" />
-            <p>10/28/12</p>
-          </div>
-          <div className="visitCard--item">
-            <BiHome color="345996" size="20" />
-            <p>MasterCard</p>
-          </div>
-          <div className="visitCard--item">
-            <BiMusic color="345996" size="20" />
-            <p>Kiribati</p>
-          </div>
-          <div className="divider"></div>
-        </div>
-        <div className="visitCard">
-          <div className="visitCard--item">
-            <BiMicrophone color="345996" size="20" />
-            <p>The Walt Disney Company</p>
-          </div>
-          <div className="visitCard--item">
-            <BiUser color="345996" size="20" />
-            <p>Ronald Richards</p>
-          </div>
-          <div className="visitCard--item">
-            <BiCalendar color="345996" size="20" />
-            <p>10/28/12</p>
-          </div>
-          <div className="visitCard--item">
-            <BiHome color="345996" size="20" />
-            <p>MasterCard</p>
-          </div>
-          <div className="visitCard--item">
-            <BiMusic color="345996" size="20" />
-            <p>Kiribati</p>
-          </div>
-          <div className="divider"></div>
-        </div>
+        <VisitCard
+          speech="¿Cómo ser amigos de Jehová?"
+          speaker="Isaac Gómez Reyes"
+          date="10/03/22"
+          congregation="Halliburton"
+          song="Una oración ferviente"
+        />
+        <VisitCard
+          speech="¿Cómo ser amigos de Jehová?"
+          speaker="Isaac Gómez Reyes"
+          date="10/03/22"
+          congregation="Halliburton"
+          song="Una oración ferviente"
+        />
+        <VisitCard
+          speech="¿Cómo ser amigos de Jehová?"
+          speaker="Isaac Gómez Reyes"
+          date="10/03/22"
+          congregation="Halliburton"
+          song="Una oración ferviente"
+        />
       </section>
     </section>
   );
