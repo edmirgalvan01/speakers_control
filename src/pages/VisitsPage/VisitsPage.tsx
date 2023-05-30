ea09a959fef81a252eb2424f3d2c103228578596
Edmir Galvan <edmirgalvaz@gmail.com>
2023-05-30 14:23:48 -0600

    Fetch visits from database

--- src/pages/VisitsPage/VisitsPage.tsx
+++ src/pages/VisitsPage/VisitsPage.tsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../../supabase/client";
 import { PrimaryButton } from "../../components/Buttons/Buttons";
 import { BackButton } from "../../components/BackButton/BackButton";
 import { VisitCard } from "../../components/VisitCard/VisitCard";
-
-import { useNavigate } from "react-router-dom";
-
+import { PostgrestError } from "@supabase/supabase-js";
+import { Visit } from "../../types/visits";
 import "./VisitsPage.css";
 
 export const VisitsPage = () => {
   const navigate = useNavigate();
+
+  const [visits, setVisits] = useState<Array<Visit>>([]);
+  const [error, setError] = useState<PostgrestError | null>();
+
+  const getVisits = async () => {
+    const { data, error } = await supabase.from("visits").select();
+
+    return { data, error };
+  };
+
+  useEffect(() => {
+    getVisits().then(({ data, error }) => {
+      setVisits(data as Array<Visit>);
+      setError(error);
+    });
+  }, []);
 
   return (
     <section className="container-page visitsPage">
@@ -17,27 +35,16 @@ export const VisitsPage = () => {
         Nueva visita
       </PrimaryButton>
       <section className="table">
-        <VisitCard
-          speech="¿Cómo ser amigos de Jehová?"
-          speaker="Isaac Gómez Reyes"
-          date="10/03/22"
-          congregation="Halliburton"
-          song="Una oración ferviente"
-        />
-        <VisitCard
-          speech="¿Cómo ser amigos de Jehová?"
-          speaker="Isaac Gómez Reyes"
-          date="10/03/22"
-          congregation="Halliburton"
-          song="Una oración ferviente"
-        />
-        <VisitCard
-          speech="¿Cómo ser amigos de Jehová?"
-          speaker="Isaac Gómez Reyes"
-          date="10/03/22"
-          congregation="Halliburton"
-          song="Una oración ferviente"
-        />
+        {visits.map((visit) => (
+          <VisitCard
+            key={visit.id}
+            speech={visit.speech_title}
+            speaker={visit.speaker_name}
+            date={visit.visit_date}
+            congregation={visit.speaker_congregation}
+            song={visit.speech_song}
+          />
+        ))}
       </section>
     </section>
   );
---
+++ src/types/visits.ts
@@ -0,0 +1,9 @@
+export interface Visit {
+  created_at?: string;
+  id?: number;
+  speaker_congregation: string;
+  speaker_name: string;
+  speech_song: string;
+  speech_title: string;
+  visit_date: string;
+}
