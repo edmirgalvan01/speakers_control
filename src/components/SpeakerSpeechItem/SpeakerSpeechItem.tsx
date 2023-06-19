d8a0e8758bfc620b69423103cbd2eafe6392990a
Edmir Galvan <edmirgalvaz@gmail.com>
2023-06-19 13:10:02 -0600

    Add button to delete a speaker speech

--- src/components/SpeakerSpeechItem/SpeakerSpeechItem.tsx
+++ src/components/SpeakerSpeechItem/SpeakerSpeechItem.tsx
@@ -1,3 +1,4 @@
+import { BiTrash } from "react-icons/bi";
 import "./SpeakerSpeechItem.css";
 
 interface Props {
@@ -9,6 +10,7 @@ export const SpeakerSpeechItem = ({ id, title }: Props) => {
   return (
     <li className="SpeakerSpeechItem" key={id}>
       <p>{title}</p>
+      <BiTrash size="20" color="F24C3D" />
     </li>
   );
 };
